9c6e83dc2e1865b98d5a1362328f3f2ab503dcf5
Cédric Rassaert <crassaert@campings.com>
2024-08-28 15:27:33 +0200

    remove middlewares

--- service/configurations/Language.js
+++ service/configurations/Language.js
@@ -1,4 +1,5 @@
 const url = require('url');
+const logger = require('pelias-logger').get('api');
 
 const _ = require('lodash');
 
@@ -10,16 +11,27 @@ class Language extends ServiceConfiguration {
   }
 
   getParameters(req, res) {
-    // find all the values for all keys with names that end with '_id'
-    const ids = _.get(res, 'data', []).reduce((acc, doc) => {
+    logger.info('get parameters for language');
+    _.get(res, 'data', []).reduce((acc, doc) => {
+      console.log(doc);
       Array.prototype.push.apply(acc, _.values(_.pickBy(doc.parent, (v, k) => _.endsWith(k, '_id') ) ) );
       return acc;
     }, []);
+    // find all the values for all keys with names that end with '_id'
+    const ids = _.get(res, 'data', []).reduce((acc, doc) => {
+
+      logger.info('doc', doc.parent);
+      Array.prototype.push.apply(acc, _.values(_.pickBy(doc.parent, (v, k) => _.endsWith(k, '_id') ) ) );
+      return acc;
+    }, []);
+
     const lang = _.get(req, 'clean.lang.iso6393');
     const parameters = {
       // arrays will be nested, so flatten first, then uniqify, and finally join elements with comma
       ids: _.uniq(_.flattenDeep(ids)).join(',')
     };
+
+    logger.info('parameters', parameters);
 
     if (lang) {
       parameters.lang = lang;
